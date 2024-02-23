be18460fe32b6e619524b264d53b3bf18932c224
meghna16694 <meghnagondaliya15@gmail.com>
2024-02-23 14:07:11 +0530

    JS

---
+++ day1(Output show Method)/script.js
@@ -0,0 +1,37 @@
+console.log("Hello World")
+
+console.log("My Name is Meghna Gondaliya")
+
+console.log("I am 28 years old")
+
+console.log("My hobby is dancing")
+
+console.log("My favourite subject is Maths")
+
+console.log("I'm currently doing a Full stack development cource ")
+
+document.write("Welcome to my Website")
+
+document.write("<h1> About me </h1> <br>")
+
+document.write("Myself <b><i> Meghna Gondaliya </i></b> <br>")
+
+document.write("I'm <b>28 years </b> old <br>")
+
+document.write("I 'm from <b>Jetpur</b> <br>")
+
+document.write("I have complete my graduation in ,<b> B.COM</b> <br>" )
+
+document.write("I have completed master degree in <b> Socialwork</b> <br>")
+
+document.write("My Marital Status is Married ,<br>")
+
+document.write("My hobbies are <b><i>Cooking and dancing</i></b> <br>")
+
+document.write("I have experience of <b>automobile industry</b> <br>")
+
+document.write("I'm currenty doing a <b>Full Stack development Cource</b> in Red nd White Multimedia Education <br>")
+
+document.getElementById('myHeader').innerHTML="Hello! New Box"
+
+alert("First Enter Your Name")
